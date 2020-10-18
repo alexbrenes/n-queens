13e42d02a439fd237dc431fece3acabe9b54bacb
Alex Brenes <brenes.brenes.alex@gmail.com>
2020-10-18 12:28:27 -0600

    v1

--- nreinas.erl
+++ nreinas.erl
@@ -2,20 +2,13 @@
 %% "■"
 %% "□"
 -module(nreinas).
--export([
- poblacion/1, shuffle/1,evaluate/1,aptitudes/1,listaColl/1,indexOf/2,elementAt/2,nextGen/3]).
--export([printI/2, fitness/1,crossOver/2,mutation/3,geneticosNReinas/1,cruce/3,cruceA/3,replaceAt/3]).
+-export([geneticosNReinas/1,cruce/3, rellenar/2]).
 
 	%% Otras funciones
 
 elementAt([], _N)->ok;
 elementAt([H|_T], 0)->H;
 elementAt([_H|T], N)->elementAt(T,N-1).
-
-indexOfAux(_E,[], _Idx)->-1;
-indexOfAux(E, [E|_T], Idx)->Idx;
-indexOfAux(E, [_H|T], Idx)-> indexOfAux(E, T, Idx + 1).
-indexOf(E,L) -> indexOfAux(E,L,0).
 
 shuffle([])->[];
 shuffle(Lista)->[A||{_B,A} <- lists:sort([{rand:uniform(), V} || V<-Lista])].
@@ -27,32 +20,45 @@ fitness(P) -> lists:sort(fun({_,A},{_,B}) -> A < B end, [{X, evaluate(X)} || X<-
 	%% Nuevo cruzamiento
 		%% Nueva generación
 
-
-
-crossOver([{E,F}|T], N) ->  mutation([E] ++ [crossOverAux([{E,F}|T], N) || _X<-lists:seq(0, N*4 - 2)],N).
+crossOver([{E,F}|T], N) ->  mutation([E] ++ [crossOverAux([{E,F}|T], N) || _X<-lists:seq(0, N*N - 2)],N).
 
 crossOverAux(Apt, N) -> A = selection(Apt, rand:uniform(N) - 1),
-%			cruce(A, selection(Apt -- [A], rand:uniform(N) - 1), rand:uniform(N-1)).
-			cruceA(element(1,lists:split(N div 2,A)), selection(Apt -- [A], rand:uniform(N) - 1),N).
+			%cruce(element(1,lists:split(N div 2, element(1,A))), element(1,selection(Apt -- [A], rand:uniform(N) - 1)), rand:uniform(N-1)).
+			cruce(element(1,lists:split(N div 2,element(1,A))), element(1,selection(Apt -- [A], rand:uniform(N) - 1)),N).
+%			cruce(element(1,lists:split(N div 2,element(1,selection(Apt, rand:uniform(N) - 1)))), element(1,selection(Apt, 	rand:uniform(N) - 1)),N).
+			%cruceA(element(1,lists:split(N div 2,element(1,selection(Apt, rand:uniform(N) - 1)))), element(1,selection(Apt, 	rand:uniform(N) - 1)),N).
+			%cruce(selection(Apt, rand:uniform(N) - 1),selection(Apt, rand:uniform(N) - 1),N).
+
+%CRUCE ++ (lists:seq(0,N-1) -- CRUCE) #################VA GANANDO
+%cruce(I1,I2,N) -> A = sets:to_list(sets:from_list(I2)),%%%%%%%%%
+%		C = I1 ++ [X || X<-(A -- I1)],%%%%%%%%%%%%%
+%		C ++ (lists:seq(0,N-1) -- C).%%%%%%%%%%%%
+		%QUITAR LO DE ABAJO
+		%B ++ rellenar(B,N).
+
+
+
+
+cruce(I1, I2, N) -> 	A = I1 ++ [X || X<-I2 ,not lists:member(X, I1)], 
+			element(1, lists:split(N, A ++ rellenar(A, N))).
+rellenar(I, N) when length(I) < N ->  lists:seq(0,N-1) -- I;
+rellenar(_,_)->[].
 %% Prueba 1
-cruce(I1,I2,S)-> element(1, lists:split(S,I1)) ++ element(2,lists:split(S,I2)).
-%% Prueba 2
-%cruce(I1,I2,S)-> element(1, lists:split(S,I1)) ++ (I2 -- element(1, lists:split(S,I1))).
+%cruce(I1,I2,S)-> element(1, lists:split(S,I1)) ++ element(2,lists:split(S,I2)).
 
 
-
-selection([{I,F}|_T], Fl) when F >= Fl -> I;
-selection([{I,_F}|[]], _Fl) -> I; % {[4,1,3,5,7,6,0,2],1}
+selection([{I,F}|_T], Fl) when F >= Fl -> {I,F};
+selection([{I,F}|[]], _Fl) -> {I,F}; % {[4,1,3,5,7,6,0,2],1}
 selection([_H|T], Fl) -> selection(T, Fl).
 
 replaceAt([], _E, _N) -> [];
 replaceAt([_H|T], E, 0) -> [E] ++ T;
 replaceAt([H|T], E, N) -> [H] ++ replaceAt(T,E, N-1).
 
-mutation(P, N) -> mutation(P, N, round(N*4 * 0.05)).
+mutation(P, N) -> mutation(P, N, round(N*N * 0.05)).
 
 mutation(P, _N, 0) -> P;
-mutation(P, N, PC) -> A1 = rand:uniform(N*4) - 1,
+mutation(P, N, PC) -> A1 = rand:uniform(N*N) - 1,
 			mutation(replaceAt(P, replaceAt(elementAt(P, A1), rand:uniform(N) - 1, rand:uniform(N) - 1), A1), N, PC - 1).
 
 
@@ -73,12 +79,6 @@ evaluate([])->0;
 evaluate(I)->evaluateAux([evaluateIdx(0,X, elementAt(I, 0),elementAt(I, X), I) || X<-lists:seq(0, length(I) - 1)]).
 
 
-%% Calcular las aptitudes de todos los individuos de la pooblación P
-aptitudes(P)->[evaluate(X) || X<-P].
-%%%%%%%%
-listaColl(I)->[evaluateIdx(0,X, elementAt(I, 0),elementAt(I, X), I) || X<-lists:seq(0, length(I) - 1)].
-%%%%%%%%
-
 	%% Imprimir tablero
 	
 printI(I,N) -> printI(I,N,0,0).
@@ -90,56 +90,22 @@ printI(In, N, I, J) -> printIAux(elementAt(In, J),I,J), printI(In, N, I, J + 1).
 printIAux(E, E, _J) ->  io:format("~ts ", [unicode:characters_to_binary("♕")]);
 printIAux(_E, I, J) when (I + J) rem 2 == 0 -> io:format("~ts ", [unicode:characters_to_binary("■")]);
 printIAux(_E, _I, _J) -> io:format("~ts ", [unicode:characters_to_binary("□")]).
-%printI([H|T], N, H, K) -> io:format("~ts ", [unicode:characters_to_binary("♕")]),
-%			   printI(T, N, H, K + 1);
-%printI(_I, N, N, _K) -> io:format("~n", []);
-%printI(I, N, J, N) ->  io:format("~n", []),
-%			printI(I,N,J+1,0);
-%printI(I, N, J, K) when (J + K) rem 2 == 0 -> io:format("~ts ", [unicode:characters_to_binary("■")]),
-%			printI(I, N, J, K + 1);
-%printI(I, N, J, K) -> io:format("~ts ", [unicode:characters_to_binary("□")]),
-%			printI(I, N, J, K + 1).
 
 	%% Población
 
 cruceA(I1,I2,N) -> element(1,lists:split(N, I1 ++ [X || X<-(I2--I1)])).
 
-	%% Siguiente generación
-nextGen(P, E, N)->
-		[E] ++ [cruceA(element(2,lists:split(N div 2, elementAt(P, rand:uniform(N*4) - 1))), elementAt(P, rand:uniform(N*4) - 1),N) 
-		|| _X<-lists:seq(0, N*4 - 2)].
-
 	%% Población Inicial
 % Esta función recibe un número natural y retorna una lista de individuos (listas de tableros)
 poblacion(0)->[];
-poblacion(N)->[shuffle(lists:seq(0,N - 1)) || _X<-lists:seq(1,N*4)].
-
-	%% Elitismo
-
-%% Obtener el individuo élite de la población P
-%%getEliteAux(P, ApEl, Idx) -> {elementAt(P, Idx), ApEl, Idx}.
-%%getElite(P,Aptitudes,ApEl) -> getEliteAux(P, ApEl, indexOf(ApEl, Aptitudes)).
-%%findElite(P, Aptitudes) -> getElite(P, Aptitudes, lists:foldl(fun(B, A) -> min(A, B) end, 100000000000, Aptitudes)).
-
-	%% Calcular diferentes generaciones
-%generacionAux(_N, _P, _Aptitudes, {E, 0, _Idx})->E;
-%generacionAux(_N, _P, _Aptitudes, E) -> E. % Solución
-%generacion(N, P, Aptitudes)->generacionAux(N, P, Aptitudes, findElite(P, Aptitudes)).
-
-	%% Función inicial
-%%geneticosNReinasAux(N, P) -> generacion(N, P, aptitudes(P)).
-%%geneticosNReinas(N) -> geneticosNReinasAux(N, poblacion(N)).
-
-
+poblacion(N)->[shuffle(lists:seq(0,N - 1)) || _X<-lists:seq(1,N*N)].
 
 %% Otras pruebas que no sé si son "ilegalísimas"
-
 geneticosNReinas(N) -> geneticosNReinas(N, fitness(poblacion(N)), 0).
 
 printaux([H|T]) -> io:format("[~p,",[H]), printauxx(T).
 printauxx([H|[]]) -> io:format("~p]~n",[H]);
 printauxx([H|T]) -> io:format("~p,",[H]), printauxx(T).
-%%solucion(E)->
 
 geneticosNReinas(N, [{E,0}|_T], G) ->
 						io:format("~p~n",["--- Solución --- Generación: " ++ integer_to_list(G)]),
@@ -148,6 +114,8 @@ geneticosNReinas(N, [{E,0}|_T], G) ->
 geneticosNReinas(N, [{E,F}|T], G) ->
 					io:format("~p~n",["--- Élite --- Generación: " ++ integer_to_list(G)]),
 					io:format("~p~n",["    Aptitud: " ++ integer_to_list(F)]),
+					io:format("~p~n",["Longitud: " ++ integer_to_list(length(E))
+					]),
 					printI(E,N),
 					printaux(E),
 					geneticosNReinas(N, fitness(crossOver([{E,F}|T], N)), G + 1).
--- nreinas.scm
+++ nreinas.scm
@@ -2,20 +2,32 @@
 ;; "\u25A0" "■"
 ;; "\u25A1" "□"
 
+;; Dominio: Un valor natural n y un símbolo e.
+;; Codominio: Una lista con n veces el símbolo e.
+
 (define repeat
   (lambda (n e)
     (cond ((= n 0) '())
           (#t (append (list e) (repeat (- n 1) e))))))
+
+;; Dominio: Una lista l, un número natural a y un símbolo b.
+;; Codominio: Una lista con el elemento b en la posición a de la lista.
 
 (define replace-at
   (lambda (l a b)
     (cond ((= a 0) (append (list b) (cdr l)))
           (#t (append (list (car l)) (replace-at (cdr l) (- a 1) b))))))
 
+;; Dominio: Una lista l y un número natural k.
+;; Codominio: El elemento k-ésimo de la lista l.
+
 (define element-at
   (lambda (l k)
     (cond ((<= k 0) (car l))
           (#t (element-at (cdr l) (- k 1))))))
+
+;; Dominio: Una lista de números desde 0 hasta a-1; dos número naturales i y j menores o iguales al tamaño de la lista.
+;; Codominio: #<void>.
 
 (define print-solution-aux
   (lambda (solution i j)
@@ -28,9 +40,16 @@
                               (#t "\u25A0 ")))
                (print-solution-aux solution i (+ j 1))))))
 
+;; Dominio: Una lista de números naturales desde 0 hasta a - 1.
+;; Codominio: #<void>.
+
 (define print-solution
   (lambda (solution)
     (print-solution-aux solution 0 0)))
+
+
+;; Dominio: Dos índices j e index y una lista de las posiciones de las reinas en el tablero.
+;; Codominio: Verdadero o falso, indica si la posición index de la reina es válida.
 
 (define evaluate-aux
   (lambda (j index table)
@@ -39,9 +58,15 @@
           ((equal? (abs (- j index)) (abs (- (element-at table j) (element-at table index)))) #f)
           (#t (evaluate-aux (+ j 1) index table)))))
 
+;; Dominio: Un índice index que indica la posición en la que se pretende colocar la reina y la lista con las otras reinas sobre el tablero.
+;; Codominio: Verdadero o falso, indica si la posición index de la reina es válida.
+
 (define evaluate
   (lambda (index table)
     (evaluate-aux 0 index table)))
+
+;; Dominio: Una lista de las posiciones de las reinas sobre el tablero, dos índices que indican la posible posición de una nueva reina en el tablero, el tamaño del tablero a, la cantidad soluciones requeridas b y la lista de soluciones encontradas. 
+;; Codominio: Una lista de soluciones donde: length(solutions) <= b.
 
 (define a-reinas
   (lambda (table index row a  b solutions)
@@ -51,6 +76,9 @@
           ((evaluate index (replace-at table index row)) (a-reinas table index (+ row 1) a b
                                                                    (a-reinas (replace-at table index row) (+ index 1) 0 a b solutions)))
           (#t (a-reinas table index (+ row 1) a b solutions)))))
+
+;; Dominio: Un tamaño del tablero o cantidad de reinas a, con a > 4, siendo a un número natural. Además, una cantidad b de soluciones deseadas.
+;; Codominio: Una lista de soluciones donde: length(solutions) <= b.
 
 (define backtrackingNReinas
   (lambda (a b)
